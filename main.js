40e526310438fe9c37a5f56dfbb9f86ea17cd162
sueun4 <121524258+sueun4@users.noreply.github.com>
2024-06-25 21:52:17 +0900

    main

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// window.addEventListener('scroll', () => { 
-
-//         var scrollTop = $(window).scrollTop();
-//         var innerHeight = $(window).innerHeight();
-//         var scrollHeight = $('body').prop('scrollHeight');
-//         console.log(scrollTop);
-//         console.log(innerHeight);
-//         console.log(scrollHeight);
-//         if (scrollTop + innerHeight >= scrollHeight) {
-//             $('#bottom').css('display','block')
-//             $('#wrap').css('position','fixed')
-//         } else {
-//             $('#bottom').css('display','none')
-//             $('#wrap').css('position','unset')
-//         }
-
-// });
-
 $('header .logo').hover(
     function(){$('header .one span').removeClass('out').addClass('in')},
         function(){$('header .one span').removeClass('in').addClass('out')}
